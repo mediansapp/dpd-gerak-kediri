6f884b029a5ef97c9fbb73b403d80ac24063a0e4
mediansapp <35758913+mediansapp@users.noreply.github.com>
2025-10-05 03:05:46 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // config.php - sesuaikan kredensial database
 $DB_HOST = 'localhost';
-$DB_USER = 'root';
-$DB_PASS = '';
-$DB_NAME = 'db_dpd_kediri';
+$DB_USER = 'b17_40091018';
+$DB_PASS = 'd14nAki;';
+$DB_NAME = 'b17_40091018_db_dpd_kediri';
 
 $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 if ($conn->connect_error) { die("Koneksi gagal: " . $conn->connect_error); }
